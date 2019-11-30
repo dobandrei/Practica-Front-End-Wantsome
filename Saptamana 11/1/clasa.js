228803235e7e758396ff71fb61fc6baafb3e818c
dobandrei <dob_andrei@yahoo.com>
2019-11-30 18:59:43 +0200

    Temele din urma

--- Saptamana 11/1/clasa.js
+++ Saptamana 11/1/clasa.js
@@ -32,14 +32,34 @@ releases.map(obj => {
 
 newReleases.filter(obj => obj.rating === 5).map(obj => obj.id);
 
+
 movieLists.map(obj => obj.videos.map(obj => obj.id));
 
 
-const moveLetters = function(string){
-    let stringArray = string.split("");
-    return stringArray.map((str,index) => String.fromCharCode(str.charCodeAt(stringArray[index])+1));
-    };
+movieLists.map(obj => obj.videos.map(obj => obj.id + " " + obj.title + " " + obj.boxarts.filter(obj => (obj.width === 150 && obj.height === 200)).map(obj => obj.url)));
 
 
+const moveLetters = string => string.split("").map((str,index) => String.fromCharCode(str.charCodeAt(stringArray[index])+1));
+ 
+
+const changeWordCase = string =>  string.split(" ").map((str,index) => (Number.isInteger(index/2)) ? str.toUpperCase() : str);  
 
 
+let allCats = arr.filter(obj => obj.species === "cat").map(obj => obj.name);  
+let allHumans = arr.filter(obj => obj.species === "human").map(obj => obj.name); 
+let allFemales = arr.filter(obj => obj.sex === "f").map(obj => obj.name); 
+let totalOfAllAges = arr.reduce((accumulator,obj) => accumulator + obj.age,0);
+let averageAgeOfCats =  arr.filter(obj => obj.species === "cat").reduce((accumulator,obj) => accumulator + obj.age,0) / allCats.length;
+let averageAgeOfHumans = arr.filter(obj => obj.species === "human").reduce((accumulator,obj) => accumulator + obj.age,0) / allHumans.length;
+let avgLengthOfNames = allCats.concat(allHumans).map(obj => obj.split("")).reduce((acc,obj) => acc + obj.length,0) / arr.map(obj => obj.name).length;
+
+
+const square = value => Math.pow(value,2);
+const double = value => value * 2;
+const composedValue = (f1,f2,value) => f1(f2(value));
+
+
+const myFunction = value => compose(square,double,value);
+
+
+const find = (array,f) => array.filter(n => f(n)).map(n=>n);
