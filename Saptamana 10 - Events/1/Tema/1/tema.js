3aee8b387aa83e6aab0e30ea5e1cdf3bf450f99e
dobandrei <dob_andrei@yahoo.com>
2019-12-01 21:44:54 +0200

    Completari la teme

--- Saptamana 10 - Events/1/Tema/1/tema.js
+++
@@ -1,6 +0,0 @@
-function buttonEventHandler () {
-    return alert("Ai apasat pe buton.");
-  } 
-  const button = document.getElementById("button");
-  button.addEventListener("click",buttonEventHandler ());
-  
