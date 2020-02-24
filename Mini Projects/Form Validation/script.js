9d47b453d0cb7d4fad0aa59738149fd5b4e8b6ab
dobandrei <dob_andrei@yahoo.com>
2020-02-24 12:53:22 +0200

    Some update to Form Validation project

--- Mini Projects/Form Validation/script.js
+++ Mini Projects/Form Validation/script.js
@@ -1,1 +1,11 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const corectPass = document.getElementById("corectPass");
 
+form.addEventListener("submit", function(e){
+    if (username.value === "") {
+        
+    }
+});
