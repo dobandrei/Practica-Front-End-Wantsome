d3735dc65dc7b6f0aee8742055b7dae5ddd74c4a
dob_andrei <dob_andrei@yahoo.com>
2020-02-25 21:46:43 +0200

    Air BnB React Project update

---
+++ ReactProjects/airbnb/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from "./components/Header";
+
+import Tabs from "./components/Tabs";
+
+import MainBody from "./components/MainBody";
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Tabs />
+    <MainBody />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactProjects/airbnb/src/components/Property.js
@@ -0,0 +1,9 @@
+let propertyObj = {
+    url : "https://r-cf.bstatic.com/images/hotel/max1024x768/173/173383105.jpg",
+    availibility : "ENTIRE",
+    type : "APARTMENT",
+    city : "ROME",
+    name : "CAMPO DE FIORI",
+    stars : "588",
+    superhost : "Superhost"
+}
